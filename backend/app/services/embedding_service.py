afa2a07ac0fb3109f0329d269cc60b4925338834
shadil <shadil@gmail.com>
2026-07-09 15:37:45 +0530

    Lazy load embedding model

--- backend/app/services/embedding_service.py
+++ backend/app/services/embedding_service.py
@@ -4,12 +4,14 @@ _model = None
 
 def get_model():
     global _model
+
     if _model is None:
+        print("Loading embedding model...")
         _model = SentenceTransformer("all-MiniLM-L6-v2")
+
     return _model
 
 
 def create_embeddings(chunks):
     model = get_model()
-    embeddings = model.encode(chunks).tolist()
-    return embeddings
+    return model.encode(chunks).tolist()
